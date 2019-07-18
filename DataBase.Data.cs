1ca9b0b07c08fcf6b07584ea09bf7f1fa835a21c
Pixeye <dimmpixeye@users.noreply.github.com>
2019-07-18 08:27:36 +0200

    Update

---
+++ DataBase.Data.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Pixeye.Framework
+{
+	public sealed class Data
+	{
+		public static int lastDataComponentID;
+		public object[] nodes = new object[6];
+
+		public T Add<T>() where T : new()
+		{
+			if (Indexer<T>.ID == -1)
+				Indexer<T>.ID = ++lastDataComponentID;
+
+			if (Indexer<T>.ID >= nodes.Length)
+				Array.Resize(ref nodes, Indexer<T>.ID << 1);
+
+			var o = new T();
+			nodes[Indexer<T>.ID] = o;
+			return o;
+		}
+		public T Add<T>(T obj)
+		{
+			if (Indexer<T>.ID == -1)
+				Indexer<T>.ID = ++lastDataComponentID;
+
+			if (Indexer<T>.ID >= nodes.Length)
+				Array.Resize(ref nodes, Indexer<T>.ID << 1);
+
+			nodes[Indexer<T>.ID] = obj;
+			return obj;
+		}
+
+		public static class Indexer<T>
+		{
+			public static int ID = -1;
+		}
+	}
+
+	public static class DBHelper
+	{
+		public static Data[] source = new Data[Entity.settings.SizeEntities];
+
+		public static Data Set(in this ent entity, Data obj)
+		{
+			if (source.Length <= entity.id)
+				Array.Resize(ref source, entity.id << 1);
+
+			source[entity.id] = obj;
+			return obj;
+		}
+	}
+}
